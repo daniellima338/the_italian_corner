e184805e144c0f1ddd6175f582886bc8edf08d8f
daniellima338 <daniellima338@gmail.com>
2022-01-05 09:54:26 +0000

    made changes to the sorting functionality

--- products/views.py
+++ products/views.py
@@ -14,7 +14,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    categories = None
+
     sort = None
     direction = None
 
@@ -52,7 +52,6 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
