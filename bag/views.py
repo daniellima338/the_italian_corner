8613c415c6850c9cd61664955e45b9111a730725
daniellima338 <daniellima338@gmail.com>
2022-01-11 12:39:02 +0000

    added toast messages to the bag view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -29,15 +29,18 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
+    
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
     
     if item_id in list(bag.keys()):
         if quantity > 0:
-            bag[item_id] += quantity
+            bag[item_id] = quantity
+            # messages.success(request, f'Updated {product.name} quantity to { bag[item_id]}') remove message are not working with this active
         else:
             bag.pop(item_id)
+            # messages.success(request, f'Removed {product.name} from your bag') remove message are not working with this active
     
         request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -45,15 +48,16 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
-
+        messages.success(request, f'Removed {product.name} from your bag')
+        
         request.session['bag'] = bag
         return HttpResponse(status=200)
-
+        
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
