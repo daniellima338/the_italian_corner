453c2dd5aa5f91bbf3779437f4f22e8b6a7500bb
daniellima338 <daniellima338@gmail.com>
2022-01-13 13:31:22 +0000

    added blog and post_detail templates and views and urls to supply

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
-class post_list(generic.ListView):
-    show_published_posts = Post.objects.filter(status=1).order_by('created_on')
-    template = 'blog.html'
+def post_list(request):
+    """ A view to return the blog page"""
+
+    """ to only show posts that are published"""
+
+    posts = Post.objects.filter(status=1).order_by('created_on')
+
+    template = 'blog/blog.html'
     
-    return render(template)
+    context = {
+        'posts': posts,
+    }
 
-class post_detail(generic.DetailView):
-    model = Post
-    template = 'post_detail.html'
+    return render(request, template, context)
 
-    return render(template)
+def post_detail(request, post_id):
+    """ A view to return the blog details """
+
+    specific_blog = request.GET.get('post_id')
+    posts = Post.objects.filter(status=1).order_by('created_on')
+
+
+    template = 'blog/post_detail.html'
+    
+    context = {
+        'specific_blog': specific_blog,
+        'posts':posts,
+    }
+
+    return render(request, template, context)
+
+
 
