5c0770da9d55809746c53cebd2ffd9702da8b055
daniellima338 <daniellima338@gmail.com>
2022-01-16 20:21:44 +0000

    deployment process complete and changed debug mode to in development

--- the_italian_corner/settings.py
+++ the_italian_corner/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_ITALIAN_CORNER', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-italian-corner.herokuapp.com', 'localhost']
 
@@ -141,7 +141,7 @@ else:
         }
     }
 
-    
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
