9a4e3afe4c211e8e88fb6f058c61f40d56f18103
daniellima338 <daniellima338@gmail.com>
2022-01-19 10:58:08 +0000

    Added class methods to webhook handler and webhook view

--- the_italian_corner/settings.py
+++ the_italian_corner/settings.py
@@ -215,6 +215,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY_THE_ITALIAN_CORNER', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY_THE_ITALIAN_CORNER', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET_THE_ITALIAN_CORNER', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
