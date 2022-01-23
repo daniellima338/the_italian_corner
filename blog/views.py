26ded12b7cd37619bf864f9464a6fc44787d89ba
daniellima338 <daniellima338@gmail.com>
2022-01-23 18:35:06 +0000

    created admin panel for the comments. Created form to fill in, for users to leave comments. Adjusted the blog view to be able to GET and POST comments.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from .forms import CommentForm
 from .models import Post
 
 def post_list(request):
@@ -21,15 +22,35 @@ def post_list(request):
 def post_detail(request, post_id):
     """ A view to return the blog details """
 
-    blog = get_object_or_404(Post, pk=post_id)
-
     template = 'blog/post_detail.html'
+    blog = get_object_or_404(Post, pk=post_id)
+    comments = blog.comments.filter(active=True)
+    new_comment = None
+
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = blog
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     
     context = {
         'blog': blog,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form
     }
 
     return render(request, template, context)
+
 
 
 
